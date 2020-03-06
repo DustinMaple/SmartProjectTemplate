2995aa2463effb00e4b3152e00db0ae136220dc5
DUSTIN <gjf66924011@gmail.com>
2020-03-06 15:07:32 +0800

    base, change, 尝试输出hello world

--- src/main/java/me/dustin/action/MyAction.java
+++ src/main/java/me/dustin/action/MyAction.java
@@ -2,11 +2,12 @@ package me.dustin.action;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
 
 public class MyAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-
+        Messages.showMessageDialog("Hello World !", "Information", Messages.getInformationIcon());
     }
 }
