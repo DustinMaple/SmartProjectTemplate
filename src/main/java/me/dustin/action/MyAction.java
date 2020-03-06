1047ebc9c5864344492dfb007898c46ab8e262ca
DUSTIN <gjf66924011@gmail.com>
2020-03-06 12:27:06 +0800

    创建SmartProjectTemplate项目基础

---
+++ src/main/java/me/dustin/action/MyAction.java
@@ -0,0 +1,12 @@
+package me.dustin.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class MyAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
